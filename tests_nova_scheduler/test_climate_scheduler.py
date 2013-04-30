ceae8641a1701e039347ce0ab097b1fabfd4c5fa
Julien Danjou <julien@danjou.info>
2013-04-30 16:15:47 +0200

    Add a basic filter for Nova
    Change-Id: I2c54418e15c4783f6f062209a7038282bcb66cd8
    Signed-off-by: Julien Danjou <julien@danjou.info>

---
+++ tests_nova_scheduler/test_climate_scheduler.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+#
+# Copyright © 2013 Julien Danjou <julien@danjou.info>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import unittest
+
+from nova.tests.scheduler import fakes
+from climate.nova import climate_filter
+
+
+class ClimateSchedulerTestCase(unittest.TestCase):
+    def test_climate_scheduler(self):
+        f = climate_filter.ClimateFilter()
+        host = fakes.FakeHostState('host1', 'node1', {})
+        filter_properties = {"scheduler_hints": {"foo": "bar"}}
+        self.assertTrue(f.host_passes(host, filter_properties))
