a5af831e60bc3690f05a1d8f64a52357069e0cc4
Nikolaj Starodubtsev <nstarodubtsev@mirantis.com>
2014-02-26 14:05:24 +0400

    Remove unecessary sys.path add from docs/config.py
    We explore, that sys.path magic in docs/conf.py is unecessary now. Also,
    removing this strings provides compatibility with future Sphinx
    releases.
    
    Change-Id: I7028297f89b2ec0a1ab7007ad2594eec2c9000c0
    closes-bug: #1285036

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -19,14 +19,6 @@ import sys
 
 import os
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../../climate'))
-sys.path.append(os.path.abspath('..'))
-sys.path.append(os.path.abspath('../bin'))
-
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
